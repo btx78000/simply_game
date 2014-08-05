a443001988b8b6a2460aaafd92e79fbf79250b3a
Paul <bompardpaul@gmail.com>
2014-08-05 15:15:05 +0200

    first commit

---
+++ js/classes/Map.js
@@ -0,0 +1,63 @@
+function Map(nom) {
+	// Création de l'objet XmlHttpRequest
+	var xhr = getXMLHttpRequest();
+		
+	// Chargement du fichier
+	xhr.open("GET", './maps/' + nom + '.json', false);
+	xhr.send(null);
+	if(xhr.readyState != 4 || (xhr.status != 200 && xhr.status != 0)) // Code == 0 en local
+		throw new Error("Impossible de charger la carte nommée \"" + nom + "\" (code HTTP : " + xhr.status + ").");
+	var mapJsonData = xhr.responseText;
+	
+	// Analyse des données
+	var mapData = JSON.parse(mapJsonData);
+	this.tileset = new Tileset(mapData.tileset);
+	this.terrain = mapData.terrain;
+	
+	// Liste des personnages présents sur le terrain.
+	this.personnages = new Array();
+}
+
+// Pour récupérer la taille (en tiles) de la carte
+Map.prototype.getHauteur = function() {
+	return this.terrain.length;
+}
+Map.prototype.getLargeur = function() {
+	return this.terrain[0].length;
+}
+
+// Pour ajouter un personnage
+Map.prototype.addPersonnage = function(perso) {
+	this.personnages.push(perso);
+}
+
+Map.prototype.dessinerMap = function(context, player) {
+
+		var ligne = this.terrain;
+		this.tileset.dessinerTile(ligne[player.x], context, 0, 0);
+		for (var i = 0; i <= ligne.length; i++) {
+			this.tileset.dessinerTile(ligne[player.y], context, i * 32, i * 32);
+		}
+			
+	
+	
+	// Dessin des personnages
+	for(var i = 0, l = this.personnages.length ; i < l ; i++) {
+		this.personnages[i].dessinerPersonnage(context);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
